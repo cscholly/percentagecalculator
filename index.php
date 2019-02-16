23fcb909c1bcbb229727cd799cfcc3bae9f77be0
Casper Scholly <caspers@scholly.co.za>
2019-02-17 00:41:34 +0200

    d

--- index.php
+++ index.php
@@ -33,6 +33,9 @@
               </div>
               <div class="col-sm">
                 <div class="card">
+                  <div class="card-body">
+                    This is some text within a card body.
+                  </div>
                 </div>
               </div>
             </div>
