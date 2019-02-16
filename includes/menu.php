e7cc40d6c7f367f6e7527c9a526e6f8682ba68da
Casper Scholly <caspers@scholly.co.za>
2019-02-17 00:11:49 +0200

    f

--- includes/menu.php
+++ includes/menu.php
@@ -1,19 +1,3 @@
-<div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="https://www.percentagecalculator.co.za">Percentage Calculator</a>
-</div>
-
-<div class="collapse navbar-collapse">
-    <ul class="nav navbar-nav">
-        <li <?php if ($page=='index.php' ) { ?>class="active"
-        <?php } ?>><a href="index.php">Percentage Calculator <span class="label label-info">New</a></a></li>
-        <li <?php if ($page=='percentage-calculator-info.php' ) { ?>class="active"
-        <?php } ?>><a href="percentage-calculator-info.php">Useful Calculation Information</a></li>
-        <li><a href="mailto:help@percentagecalculator.co.za">Suggestions?</a></li>
-    </ul>
-</div>
+<nav class="navbar fixed-top navbar-light bg-light">
+  <span class="navbar-brand mb-0 h1">Navbar</span>
+</nav>
--- index.php
+++ index.php
@@ -14,29 +14,9 @@
 
     <?php include ( 'includes/header.php'); ?>
 
-    <nav class="navbar fixed-top navbar-light bg-light">
-      <span class="navbar-brand mb-0 h1">Navbar</span>
-    </nav>
-
-
-    <!-- <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="#">Features</a>
-                <a class="nav-item nav-link" href="#">Pricing</a>
-                <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-              </div>
-            </div>
-          </nav>
-        </div>
-    </nav> -->
+    <!-- Menu start -->
+    <?php include ( 'includes/menu.php'); ?>
+    <!-- Menu end -->
 
     <div id=wrap>
         <div class="container">
