12e9ffe954fe1a650b72e62db1ab3dae87baf51b
Casper Scholly <caspers@scholly.co.za>
2019-02-16 22:44:10 +0200

    Added Privacy Policy

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <?php
-echo '<p class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0 | <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a></p>';
+echo '<p class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0 | <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="https://www.percentagecalculator.co.za/privacy-policy.php">Privacy Policy</a></p>';
 ?>
