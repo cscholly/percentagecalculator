8a4a72edce9a811edac0d47aba1c867a24a93284
Casper Scholly <caspers@scholly.co.za>
2019-02-17 00:17:51 +0200

    cards

--- index.php
+++ index.php
@@ -26,11 +26,11 @@
 
             <div class="row">
               <div class="col-md-6">
-                <div class="panel panel-default">
-                  <div class="panel-heading">
-                    <h3 class="panel-title">Percentage Calculator</h3>
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">Percentage Calculator</h3>
                   </div>
-                  <div class="panel-body">
+                  <div class="card-body">
                     <form id="calcForm">
                       <div id="calcbox">
                         <div class="form-group" style="margin-bottom: 0px;">
