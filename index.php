8be61ee4d47b2636aa1bae750abc0523a5a4419e
Casper Scholly <caspers@scholly.co.za>
2019-02-17 00:03:05 +0200

    menu

--- index.php
+++ index.php
@@ -14,15 +14,32 @@
 
     <?php include ( 'includes/header.php'); ?>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" role="navigation">
         <div class="container">
-            <?php include ( 'includes/menu.php'); ?>
-            <!--.nav-collapse -->
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="https://www.percentagecalculator.co.za">Percentage Calculator</a>
+          </div>
+
+          <div class="collapse navbar-collapse">
+              <ul class="nav navbar-nav">
+                  <li <?php if ($page=='index.php' ) { ?>class="active"
+                  <?php } ?>><a href="index.php">Percentage Calculator <span class="label label-info">New</a></a></li>
+                  <li <?php if ($page=='percentage-calculator-info.php' ) { ?>class="active"
+                  <?php } ?>><a href="percentage-calculator-info.php">Useful Calculation Information</a></li>
+                  <li><a href="mailto:help@percentagecalculator.co.za">Suggestions?</a></li>
+              </ul>
+          </div>
         </div>
     </nav>
 
     <div id=wrap>
-        <div class="container">
+        <div class="container-fluid">
             <div class="jumbotron" style="margin-top:30px">
                 <h2>Calculate how much a % of an amount is:</h2>
             </div>
