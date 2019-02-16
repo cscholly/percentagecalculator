ab7938feeffbc53be4421aee292b3adeaa9f2ba4
Casper Scholly <caspers@scholly.co.za>
2019-02-16 23:54:42 +0200

    foot update

--- includes/footer.php
+++ includes/footer.php
@@ -3,5 +3,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
 <?php
-echo '<p class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0 | <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="https://www.percentagecalculator.co.za/privacy-policy.php">Privacy Policy</a></p>';
+<p class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0 | <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="https://www.percentagecalculator.co.za/privacy-policy.php">Privacy Policy</a></p>
 ?>
