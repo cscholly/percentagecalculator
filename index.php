88d1d5f729eed5028dc9c623d891a9ee8cded373
Casper Scholly <caspers@scholly.co.za>
2019-02-17 00:41:07 +0200

    d

--- index.php
+++ index.php
@@ -33,27 +33,28 @@
               </div>
               <div class="col-sm">
                 <div class="card">
-                  <div class="card-body text-center">
-                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- PAYE - Responsive -->
-                    <ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="ca-pub-0590369945323371"
-                    data-ad-slot="8869708491"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                    <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                  </div>
                 </div>
               </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="card-body text-center">
+              <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+              <!-- PAYE - Responsive -->
+              <ins class="adsbygoogle"
+              style="display:block"
+              data-ad-client="ca-pub-0590369945323371"
+              data-ad-slot="8869708491"
+              data-ad-format="auto"
+              data-full-width-responsive="true"></ins>
+              <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+              </script>
             </div>
           </div>
         </div>
       </div>
     </div>
-
       <!-- wrap end -->
 
     <!-- footer start -->
