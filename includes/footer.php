fc7c42d68499c27bf1e067d6ae89af3a595370ed
Casper Scholly <caspers@scholly.co.za>
2019-02-17 13:41:31 +0200

    test reflow

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,4 @@
 
-<script>
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
-</script>
-
 <footer class="footer mt-auto py-3">
   <div class="container">
     <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0 | <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="https://www.percentagecalculator.co.za/privacy-policy.php">Privacy Policy</a>
--- index.php
+++ index.php
@@ -26,7 +26,7 @@
       </div>
       <div class="container">
         <div class="row">
-          <div class="col">
+          <div class="col-sm">
             <div class="card">
               <div class="card-header">
                 Quote
@@ -39,7 +39,7 @@
               </div>
             </div>
           </div>
-          <div class="col">
+          <div class="col-sm">
             <div class="card">
               <div class="card-header">
                 Quote
