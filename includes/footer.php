917f19376c7e7e23c3a537f189e047ce6ede4f8f
Casper Scholly <caspers@scholly.co.za>
2019-02-17 14:02:39 +0200

    jquery sequence changes

--- includes/footer.php
+++ includes/footer.php
@@ -6,5 +6,4 @@
   </div>
 </footer>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
