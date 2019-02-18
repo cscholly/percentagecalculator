5d1312939ff46d5a59de969edd2d4c7b52ebe2fc
Casper Scholly <caspers@scholly.co.za>
2019-02-18 19:06:03 +0200

    d

--- index.php
+++ index.php
@@ -31,10 +31,13 @@
                 Enter Your Question
               </div>
               <div class="card-body">
-                <blockquote class="blockquote mb-0">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                  <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                </blockquote>
+                <div class="input-group">
+                  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                  <div class="input-group-append">
+                    <span class="input-group-text">$</span>
+                    <span class="input-group-text">0.00</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
