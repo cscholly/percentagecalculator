46c9f39b5690c439c2d7fbf1e4875fdcdaefef9c
Casper Scholly <caspers@scholly.co.za>
2019-02-18 18:11:11 +0200

    d

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,10 @@
 
+<script>
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+</script>
+
 <footer class="footer mt-auto py-3">
   <div class="container">
     <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0 | <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="https://www.percentagecalculator.co.za/privacy-policy.php">Privacy Policy</a>
@@ -9,10 +15,6 @@
 </footer>
 
 
-<script>
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
-</script>
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
