5035b18d0e039f0597446190464c3cd0ee717e72
Casper Scholly <caspers@scholly.co.za>
2019-02-20 22:10:04 +0200

    bottom card text

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
               </div>
               <div class="card-body">
                 <p class="lead">
-                  <span id="percentageResult">%</span> of <span id="amountResult">0</span> is <span id="answer">0</span>.
+                  <span id="percentageResult">%</span> of <span id="amountResult">0</span> is <span id="answer">0</span>%.
                 </p>
               </div>
             </div>
@@ -73,12 +73,11 @@
           <div class="col">
             <div class="card">
               <div class="card-header">
-                Quote
+                Note
               </div>
               <div class="card-body">
                 <blockquote class="blockquote mb-0">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                  <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                  <p>This is just a first attempt at doing this page and calculation. More changes to come.</p>
                 </blockquote>
               </div>
             </div>
