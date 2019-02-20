fd6964e3f5a04ee005e3f70271ed916ffd34eec2
Casper Scholly <caspers@scholly.co.za>
2019-02-20 22:10:53 +0200

    footer update

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,8 @@
 <footer class="footer mt-auto py-3">
   <div class="container">
-    <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0 | <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="/privacy-policy.php">Privacy Policy</a>
+    <!-- <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0 | <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="/privacy-policy.php">Privacy Policy</a>
+    </span> -->
+    <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0
     </span>
   </div>
 </footer>
