2b69328698c769b568deddca530f100de4222ef0
Casper Scholly <caspers@scholly.co.za>
2019-02-20 22:11:28 +0200

    d

--- includes/menu.php
+++ includes/menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Percentage Calculator</a>
+  <a class="navbar-brand" href="#">Percentage Calculator Experiment</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -8,9 +8,9 @@
       <li class="nav-item active">
         <a class="nav-link" href="#">Try it! <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link" href="/percentage-calculator-info.php">Percentage Calculator Info</a>
-      </li>
+      </li> -->
     </ul>
     <span class="navbar-text">
       Easily Calculate the % of any amount
