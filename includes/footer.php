7aa6c710f23200b266c813995e21077e23f13c88
Casper Scholly <caspers@scholly.co.za>
2019-02-20 21:39:39 +0200

    footer update

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,9 @@
-
 <footer class="footer mt-auto py-3">
   <div class="container">
-    <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0 | <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="https://www.percentagecalculator.co.za/privacy-policy.php">Privacy Policy</a>
-</span>
-
+    <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0 | <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="/privacy-policy.php">Privacy Policy</a>
+    </span>
   </div>
 </footer>
-
 
 <!-- Initialize Popover -->
 <script>
@@ -14,6 +11,5 @@ $(document).ready(function() {
   $('[data-toggle="popover"]').popover();
 });
 </script>
-
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
