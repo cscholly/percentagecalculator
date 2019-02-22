bc120607f726b2267bda0e39ab1f7db23b51a78c
Casper Scholly <caspers@scholly.co.za>
2019-02-22 18:51:27 +0200

    d

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,14 @@
 <footer class="footer mt-auto py-3">
   <div class="container">
     <p><span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0.1 |
-      <a href data-toggle="popover" data-placement="top" title="Disclaimer Notice" data-content="This percentage calculator tool is just for entertainment purposes.">Disclaimer</a>
+      <a tabindex="0" data-toggle="popover" data-trigger="focus" data-placement="top" title="Disclaimer Notice" data-content="This percentage calculator tool is just for entertainment purposes.">Disclaimer</a>
     </span> | <a href="/privacy-policy.php">Privacy Policy</a> </p>
   </div>
 </footer>
 
 <!-- Initialize Popover -->
 <script>
-$(function () {
-  $('[data-toggle="popover"]').popover()
+$('.popover-dismiss').popover({
+  trigger: 'focus'
 })
 </script>
