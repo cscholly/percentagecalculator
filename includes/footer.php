e1afdc677e00c3ea9e334d7c1e62fddf1d610acb
Casper Scholly <caspers@scholly.co.za>
2019-02-22 18:33:57 +0200

    g

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,8 @@
 <footer class="footer mt-auto py-3">
   <div class="container">
-    <!-- <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0.1 |
-    <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="/privacy-policy.php">Privacy Policy</a>
-    </span> -->
-    <a type="button" class="btn btn-lg btn-danger" data-toggle="popover" data-placement="top" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</a>
-    <!-- <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0.1
-    </span> -->
+    <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0.1 |
+      <a type="button" class="btn btn-lg btn-danger" data-toggle="popover" data-placement="top" title="Popover title" data-content="This percentage calculator tool is just for entertainment purposes.">Disclaimer</a>
+    </span>
   </div>
 </footer>
 
