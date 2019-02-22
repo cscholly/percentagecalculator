10c3fac09e5286bf5180b3d2bb0997da544bd455
Casper Scholly <caspers@scholly.co.za>
2019-02-22 18:14:27 +0200

    active states on menu

--- includes/menu.php
+++ includes/menu.php
@@ -5,12 +5,10 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Try it! <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/percentage-calculator-info.php">Percentage Calculator Info</a>
-      </li>
+      <li <?php if ($page=='index.php' ) { ?>class="nav-item active"
+      <?php } ?>><a class="nav-link" href="index.php">Try it!</a></li>
+      <li <?php if ($page=='percentage-calculator-info.php' ) { ?>class="nav-item active"
+      <?php } ?>><a class="nav-link" href="percentage-calculator-info.php">Percentage Calculator Info</a></li>
     </ul>
     <span class="navbar-text">
       Easily Calculate the % of any amount
