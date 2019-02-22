b0e5be951ad9d3764e4e0d8c9f8f1a9c9c149690
Casper Scholly <caspers@scholly.co.za>
2019-02-22 19:22:09 +0200

    d

--- percentage-calculator-info.php
+++ percentage-calculator-info.php
@@ -22,22 +22,23 @@
       <div class="container">
         <div class="row" style="margin-top:30px">
           <div class="col">
-              <p>When you say "Percent" you are actually saying "per 100" or /100 or % (see the 100 in the symbol?)</p>
-              <h2>Ok, so how does this work? Let's break it down into its basic form:</h2>
-              <h3>What is P percent of A?</h3>
+            <p>When you say "Percent" you are actually saying "per 100" or /100 or % (see the 100 in the symbol?)</p>
+            <h2>Ok, so how does this work? Let's break it down into its basic form:</h2>
+            <h3>What is P percent of A?</h3>
 
-              <p>Written as an equation: B = P% * A</p>
-              <p>The 'what' is B that we want to solve.</p>
-              <p>First convert percentage to decimal, dividing by 100</p>
-              <p>Solution: Solve B using the percentage formula</p>
-              <p>B = P% * A</p>
+            <p>Written as an equation: B = P% * A</p>
+            <p>The 'what' is B that we want to solve.</p>
+            <p>First convert percentage to decimal, dividing it by 100,</p>
+            <p>Then multiply that amount by A</p>
+            <p>Like so: B = P% * A</p>
 
-              <h3>Example: What is 15% of 35?</h3>
+            <h3>Example: What is 15% of 35?</h3>
 
-              <p>Written using the percentage formula: B = 15% * 35</p>
-              <p>First convert percentage to a decimal 15/100 = 0.15</p>
-              <p>B = 0.15 * 35 = 5.25</p>
-              <p>So 15% of 35 is 5.25</p>
+            <p>Written using the percentage formula: B = 15% * 35</p>
+            <p>First convert percentage to a decimal 15/100 = 0.15</p>
+            <p>The multiply 0.15 with the amount 35</p>
+            <p>B = 0.15 * 35 = 5.25</p>
+            <p>So 15% of 35 is 5.25</p>
           </div>
         </div>
       </div>
