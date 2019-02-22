6243d79bdefae1caa5de42d83b2468e748b28ea6
Casper Scholly <caspers@scholly.co.za>
2019-02-22 19:12:50 +0200

    styling

--- percentage-calculator-info.php
+++ percentage-calculator-info.php
@@ -24,20 +24,20 @@
           <div class="col">
             <blockquote class="blockquote mb-0">
               <p>When you say "Percent" you are actually saying "per 100" or /100 or % (see the 100 in the symbol?)</p>
-              <p>Ok, so how does this work? Let's break it down into its basic form:</p>
-              <p>What is P percent of X?</p>
+              <h2>Ok, so how does this work? Let's break it down into its basic form:</h2>
+              <h3>What is P percent of A?</h3>
 
-              <p>Written as an equation: Y = P% * X</p>
-              <p>The 'what' is Y that we want to solve for</p>
+              <p>Written as an equation: B = P% * A</p>
+              <p>The 'what' is B that we want to solve for</p>
               <p>Remember to first convert percentage to decimal, dividing by 100</p>
-              <p>Solution: Solve for Y using the percentage formula</p>
-              <p>Y = P% * X</p>
+              <p>Solution: Solve for B using the percentage formula</p>
+              <p>B = P% * A</p>
 
-              <p>Example: What is 10% of 25?</p>
+              <h3>Example: What is 10% of 25?</h3>
 
-              <p>Written using the percentage formula: Y = 10% * 25</p>
+              <p>Written using the percentage formula: B = 10% * 25</p>
               <p>First convert percentage to a decimal 10/100 = 0.1</p>
-              <p>Y = 0.1 * 25 = 2.5</p>
+              <p>B = 0.1 * 25 = 2.5</p>
               <p>So 10% of 25 is 2.5</p>
             </blockquote>
           </div>
