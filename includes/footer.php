4b898717e0d61c39ee4b4ee35426212edb41dfd3
Casper Scholly <caspers@scholly.co.za>
2019-02-22 18:34:39 +0200

    g

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer mt-auto py-3">
   <div class="container">
     <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0.1 |
-      <a type="button" class="btn btn-lg btn-danger" data-toggle="popover" data-placement="top" title="Popover title" data-content="This percentage calculator tool is just for entertainment purposes.">Disclaimer</a>
+      <a type="button" data-toggle="popover" data-placement="top" title="Popover title" data-content="This percentage calculator tool is just for entertainment purposes.">Disclaimer</a>
     </span>
   </div>
 </footer>
