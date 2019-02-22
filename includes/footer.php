510d6cd7864d47f5897769d51630267ace93427e
Casper Scholly <caspers@scholly.co.za>
2019-02-22 18:37:14 +0200

    d

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <footer class="footer mt-auto py-3">
   <div class="container">
-    <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0.1 |
+    <p><span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0.1 |
       <a data-toggle="popover" data-placement="top" title="Disclaimer Notice" data-content="This percentage calculator tool is just for entertainment purposes.">Disclaimer</a>
-    </span>
+    </span> | <a href="https://www.vatcalculator.co.za/privacy-policy.php">Privacy Policy</a> </p>
   </div>
 </footer>
 
