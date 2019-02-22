1a22d58c487dc43f11e220299acc91b951d87c5e
Casper Scholly <caspers@scholly.co.za>
2019-02-22 18:31:39 +0200

    s

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
     <!-- <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0.1 |
     <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Important Information" data-placement="top" data-content="This calculator tool is just for entertainment purposes.">Disclaimer</a> | <a href="/privacy-policy.php">Privacy Policy</a>
     </span> -->
-    <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" data-placement="top" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
+    <a type="button" class="btn btn-lg btn-danger" data-toggle="popover" data-placement="top" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</a>
     <!-- <span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0.1
     </span> -->
   </div>
