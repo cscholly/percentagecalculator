809cd89ca9801fca80033814a4463f39348c2432
Casper Scholly <caspers@scholly.co.za>
2019-02-22 18:46:25 +0200

    d

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer mt-auto py-3">
   <div class="container">
     <p><span class="text-muted">All Rights Reserved | Copyright &copy; 2019 | v1.0.1 |
-      <a role="button" data-toggle="popover" data-placement="top" title="Disclaimer Notice" data-content="This percentage calculator tool is just for entertainment purposes.">Disclaimer</a>
+      <span data-toggle="popover" data-placement="top" title="Disclaimer Notice" data-content="This percentage calculator tool is just for entertainment purposes."><a>Disclaimer</a></span>
     </span> | <a href="/privacy-policy.php">Privacy Policy</a> </p>
   </div>
 </footer>
