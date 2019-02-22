48378475c5c739649499b206296400eacc84deba
Casper Scholly <caspers@scholly.co.za>
2019-02-22 18:04:48 +0200

    nav for info page

--- includes/menu.php
+++ includes/menu.php
@@ -8,9 +8,9 @@
       <li class="nav-item active">
         <a class="nav-link" href="#">Try it! <span class="sr-only">(current)</span></a>
       </li>
-      <!-- <li class="nav-item">
+      <li class="nav-item">
         <a class="nav-link" href="/percentage-calculator-info.php">Percentage Calculator Info</a>
-      </li> -->
+      </li>
     </ul>
     <span class="navbar-text">
       Easily Calculate the % of any amount
